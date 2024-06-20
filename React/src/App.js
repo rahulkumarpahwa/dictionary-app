a9443c845312fd0363cda8381455dcd4e5384d23
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-06-20 10:17:48 +0530

    created react side of the app

---
+++ React/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const App = ()=>{
+return <h1>Hello</h1>
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App/>)
